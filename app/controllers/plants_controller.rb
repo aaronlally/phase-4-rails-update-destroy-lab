1652d096aee597166ba8fb64d9a6e805421e94a1
Aaron Lally <aaronblally@gmail.com>
2022-07-02 14:47:44 -0400

    commit tree aa6f540ec4f5c870b34966c69640907ed8b32ddb

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /plants
   def index
     plants = Plant.all
@@ -8,8 +8,22 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
+  end
+
+# PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+# DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   # POST /plants
@@ -22,5 +36,13 @@ class PlantsController < ApplicationController
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
